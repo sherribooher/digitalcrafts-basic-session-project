bc5518e5a79f3e0f4c823e48d3fe46e2376fbb6b
Sherri Booher <sherribooher74@gmail.com>
2019-08-28 21:22:35 -0400

    Add new column to db for user's first name

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
     email: DataTypes.STRING,
-    password_hash: DataTypes.STRING
+    password_hash: DataTypes.STRING,
+    firstName: DataTypes.STRING
   }, {});
   user.associate = function (models) {
     // associations can be defined here
